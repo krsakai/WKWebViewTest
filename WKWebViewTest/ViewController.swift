74241d4c38c69fc8862f2a488c657757687880cb
酒井邦也 <sakaikuniya@sakaikuninarinoMacBook-Pro.local>
2017-12-17 05:09:44 +0900

    test

--- WKWebViewTest/ViewController.swift
+++ WKWebViewTest/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // create the url-request
+        let urlString = "https://floating-cliffs-24541.herokuapp.com"
+        let request = NSMutableURLRequest(url: URL(string: urlString)!)
+        
+        // set the method(HTTP-POST)
+        request.httpMethod = "POST"
+        // set the header(s)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        // set the request-body(JSON)
+        let params: [String: String] = [
+            "post-text": "test"
+        ]
+        request.httpBody = try! JSONSerialization.data(withJSONObject: params)
+        
+        let task = URLSession.shared.dataTask(with: request as URLRequest, completionHandler: {data, response, error in
+            if (error == nil) {
+                let result = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)!
+                DispatchQueue.main.async {
+                    self.webView.loadHTMLString(result as String, baseURL: request.url)
+                }
+            }
+        })
+        task.resume()
+//        webView.load(request as URLRequest)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+extension ViewController: WKUIDelegate, WKNavigationDelegate {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Swift.Void) {
+        
+    }
+}
