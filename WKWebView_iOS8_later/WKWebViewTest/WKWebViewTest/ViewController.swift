ff5c2dfdc96ae8727c5d305dc7dce42c93bfb23d
k-sakai <k_sakai@waku-2.com>
2017-12-17 17:21:51 +0900

    test

--- WKWebView_iOS8_later/WKWebViewTest/WKWebViewTest/ViewController.swift
+++ WKWebView_iOS8_later/WKWebViewTest/WKWebViewTest/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
         let webConfiguration = WKWebViewConfiguration()
         webView = WKWebView(frame: .zero, configuration: webConfiguration)
         webView.uiDelegate = self
+        webView.navigationDelegate = self
         view = webView
         // create the url-request
         let urlString = "https://floating-cliffs-24541.herokuapp.com"
@@ -29,6 +30,8 @@ class ViewController: UIViewController {
         request.httpMethod = "POST"
         // set the header(s)
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("RBD", forHTTPHeaderField: "X-APP")
+        request.addValue("1", forHTTPHeaderField: "version")
         
         // set the request-body(JSON)
         let params: [String: String] = [
@@ -36,21 +39,33 @@ class ViewController: UIViewController {
         ]
         request.httpBody = try! JSONSerialization.data(withJSONObject: params)
         
+        taskExcute(request: request as URLRequest)
+        //webView.load(request as URLRequest)
+    }
+    
+    fileprivate func taskExcute(request: URLRequest) {
         let task = URLSession.shared.dataTask(with: request as URLRequest, completionHandler: {data, response, error in
             if (error == nil) {
                 let result = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)!
                 DispatchQueue.main.async {
-                    //                    self.webView.loadHTMLString(result as String, baseURL: request.url)
+                    self.webView.loadHTMLString(result as String, baseURL: request.url)
                 }
             }
         })
         task.resume()
-        webView.load(request as URLRequest)
     }
 }
 
 extension ViewController: WKUIDelegate, WKNavigationDelegate {
     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Swift.Void) {
-        
+
+        guard let _ = navigationAction.request.allHTTPHeaderFields?["RBD"] else {
+            var request = navigationAction.request
+            request.addValue("RBD", forHTTPHeaderField: "X-APP")
+            request.addValue("1", forHTTPHeaderField: "version")
+            taskExcute(request: request as URLRequest)
+            return decisionHandler(.cancel)
+        }
+        return decisionHandler(.allow)
     }
 }
