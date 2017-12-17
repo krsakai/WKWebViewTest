b6c2b6bef7bb2831a76f683e9f7994563903305c
k-sakai <k_sakai@waku-2.com>
2017-12-17 16:35:56 +0900

    test

--- WKWebView_iOS8_later/WKWebViewTest/WKWebViewTest/ViewController.swift
+++ WKWebView_iOS8_later/WKWebViewTest/WKWebViewTest/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  WKWebViewTest
+//  WKWebViewTest1
 //
 //  Created by 酒井邦也 on 2017/12/17.
 //  Copyright © 2017年 酒井邦也. All rights reserved.
@@ -10,11 +10,17 @@ import UIKit
 import WebKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var webView: WKWebView!
+    
+    var webView: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        webView = WKWebView()
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
         // create the url-request
         let urlString = "https://floating-cliffs-24541.herokuapp.com"
         let request = NSMutableURLRequest(url: URL(string: urlString)!)
@@ -34,12 +40,12 @@ class ViewController: UIViewController {
             if (error == nil) {
                 let result = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)!
                 DispatchQueue.main.async {
-                    self.webView.loadHTMLString(result as String, baseURL: request.url)
+                    //                    self.webView.loadHTMLString(result as String, baseURL: request.url)
                 }
             }
         })
         task.resume()
-//        webView.load(request as URLRequest)
+        webView.load(request as URLRequest)
     }
 }
 
